c072d4717770a4ae4f1187395b7044e6f61d99c9
Luis Gerardo <gerardomartinez1999.gm@gmail.com>
2023-02-24 21:00:57 -0600

    added sudo as method

--- scripts/copy-files.sh
+++ scripts/copy-files.sh
@@ -1,5 +1,2 @@
 cd /home/marlo/devops
 sudo cp -r build/* nginx
-
-# sudo cp ansible/deployments-services.yml /home/abdelali/
-# sudo scp ansible/deployments-services.yml ubuntu@10.10.3.212:/home/ubuntu/cluster
